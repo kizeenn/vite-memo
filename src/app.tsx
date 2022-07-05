81a53c15f840e107ee4313745de037f9f0b0f2b3
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-07-05 15:41:06 +0200

    Install tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import MemoGame from "vite-memo/components/games/memo/index";
 import "./styles/global.css";
+import "./styles/index.css";
 
 export default function App() {
   return <MemoGame />;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import VZN from "vite-plugin-vzn";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   build: {
     target: "esnext",
   },
-  plugins: [tsconfigPaths(), VZN()],
+  plugins: [tsconfigPaths(), VZN(), tailwindcss()],
 });
