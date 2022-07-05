4cf649b9ed8a071036dc83a339d006ac46261949
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-07-05 16:21:15 +0200

    Add classes not supported by tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,44 @@
+const plugin = require("tailwindcss/plugin");
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+  });
+});
+
+const backfaceVisibility = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".backface-visible": {
+      "backface-visibility": "visible",
+    },
+    ".backface-hidden": {
+      "backface-visibility": "hidden",
+    },
+  });
+});
+
+const transformStyle = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".transform-preserve": {
+      "transform-style": "preserve-3d",
+    },
+  });
+});
+
+const perspective = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".perspective-1k": {
+      perspective: "1000px",
+    },
+  });
+});
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [rotateY, backfaceVisibility, transformStyle, perspective],
 };
